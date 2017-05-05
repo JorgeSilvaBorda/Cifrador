324846d216d267ca3780ac28cdcd0a5575d190f8
NT_BANCO\jsilvab <jsilvab.externo@bancofalabella.cl>
2017-05-05 14:39:07 -0300

    Creación de estructura inicial de la librería. Clases y constructores vacíos.

---
+++ src/com/modelo/Md5.java
@@ -0,0 +1,16 @@
+package com.modelo;
+
+/**
+ * Convierte frases a Hash MD5.
+ * @author Jorge Silva Borda
+ */
+public class Md5 {
+    
+    /**
+     * Constructor vacío.
+     */
+    public Md5(){
+	
+    }
+    
+}
---
+++ src/com/modelo/Rsa.java
@@ -0,0 +1,15 @@
+package com.modelo;
+
+/**
+ * Cifra frases por medio de llave compartida.
+ * @author Jorge Silva Borda
+ */
+public class Rsa {
+    
+    /**
+     * Constructor vacío.
+     */
+    public Rsa(){
+	
+    }
+}
