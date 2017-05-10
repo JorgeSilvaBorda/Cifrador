f1b16a23cc7da05fbc580272830d601a7ded0c3e
NT_BANCO\jsilvab <jsilvab.externo@bancofalabella.cl>
2017-05-10 13:31:58 -0300

    Se crea el Feature SHA 256 Base 64

--- src/com/modelo/Sha.java
+++ src/com/modelo/Sha.java
@@ -34,4 +34,12 @@ public class Sha {
 	byte[] encoded = Base64.getEncoder().encode(digest);
 	return new String(encoded);
     }
+    
+    public String cifrarSha256_Base64(String mensaje) throws NoSuchAlgorithmException {
+	MessageDigest md = MessageDigest.getInstance("SHA-256");
+	md.update(mensaje.getBytes());
+	byte[] digest = md.digest();
+	byte[] encoded = Base64.getEncoder().encode(digest);
+	return new String(encoded);
+    }
 }
